fc7cb0807d845841efa953c8be924d2ea73dd282
danya shuvalov <83293880+shuevalov@users.noreply.github.com>
2025-01-11 10:27:15 +0300

    chore: optimize imports in main activity

--- app/src/main/java/ru/shuevalov/eco_alpha_test_task/MainActivity.kt
+++ app/src/main/java/ru/shuevalov/eco_alpha_test_task/MainActivity.kt
@@ -4,16 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.shuevalov.eco_alpha_test_task.ui.AppScaffold
-import ru.shuevalov.eco_alpha_test_task.ui.screens.home.HomeScreen
-import ru.shuevalov.eco_alpha_test_task.ui.theme.EcoalphatesttaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
